6a113174d79b3ad7ff6f3920fcd6ec932cfdbc10
Lucas <>
2018-09-03 02:03:15 +0800

    added animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-profile></app-profile>
+    <app-profile [@enter]="'in'"></app-profile>
   `,
-  animations: [],
+  animations: [
+    trigger('enter', [
+      state('in', style({transform: 'translateY(0)'})),
+      transition('void => *', [
+        style({transform: 'translateY(-100%)'}),
+        animate('2s cubic-bezier(0.2,1,0.3,1)')
+      ])
+    ])
+  ],
   styles: [`
     :host {
       display: flex;
